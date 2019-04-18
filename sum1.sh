2bafb2ad553ffdda325de3ddc817b37155e7c140
King-tu <1079450790@qq.com>
2019-04-18 12:36:55 +0800

    commit

--- sum1.sh
+++ sum1.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+#
+# Program:
+#	Calculate the sum between 1 and the number entered by the user.
+# 2019/04/18	Kingtu	Second release
+#
+
 sum=0
 
-read -p "please input a number: " num
+while [ $sum < 1 ]
+do
+	read -p "please input a number: " num
+done
 
 for i in `seq 1 $num`
 do
